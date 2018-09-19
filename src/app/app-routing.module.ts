3003f43d3e1cfdccd9c1bc6fb87483954e3311fd
Pabrick <pabrick.bastaky@gmail.com>
2018-09-19 20:06:32 +0200

    +ADD: header is finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WorkExperienceComponent } from './views/work-experience/work-experience.component';
+import { SideProjectsComponent } from './views/side-projects/side-projects.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: '',
+    component: WorkExperienceComponent
+  },
+  {
+    path: 'experience',
+    component: WorkExperienceComponent
+  },
+  {
+    path: 'projects',
+    component: SideProjectsComponent
+  }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './views/header/header.component';
+import { FooterComponent } from './views/footer/footer.component';
+import { AboutComponent } from './views/about/about.component';
+import { ToolsComponent } from './views/tools/tools.component';
+import { MenuComponent } from './views/menu/menu.component';
+import { WorkExperienceComponent } from './views/work-experience/work-experience.component';
+import { SideProjectsComponent } from './views/side-projects/side-projects.component';
+import { TimelineComponent } from './components/timeline/timeline.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    ToolsComponent,
+    MenuComponent,
+    WorkExperienceComponent,
+    SideProjectsComponent,
+    TimelineComponent
   ],
   imports: [
     BrowserModule,
