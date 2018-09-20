35d37ce6f2a0ab227a5fe786dab0b89e58321062
Pabrick <pabrick.bastaky@gmail.com>
2018-09-20 13:30:20 +0200

    +ADD: footer is finished

--- src/app/views/footer/footer.component.ts
+++ src/app/views/footer/footer.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  private map: Map<string, string>;
+
   constructor() { }
 
   ngOnInit() {
+    this.map = new Map<string, string>();
+    this.map.set('email', 'mailto:pjbeneyto@gmail.com');
+    this.map.set('twitter', 'http://twitter.com/Pabrick');
+    this.map.set('github', 'http://github.com/Pabrick');
+    this.map.set('linkedin', 'http://www.linkedin.com/in/pabrick');
+  }
+
+  navigateTo(key) {
+    window.open(this.map.get(key), '_blank');
   }
 
 }
